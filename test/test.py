bdd6ea8f75c448c4cb98351ec744fd72e9f31ff1
YGDES <262647190+ygdes@users.noreply.github.com>
2026-04-06 01:24:05 +0200

    updated all files in /test

--- test/test.py
+++ test/test.py
@@ -1,40 +1,313 @@
 # SPDX-FileCopyrightText: © 2024 Tiny Tapeout
 # SPDX-License-Identifier: Apache-2.0
 
+# Test the Hammer18 & gPEAC scrambler & descrambler in direct, encoding and decoding modes.
+
+enable_bypass = True
+enable_Hammer_encode = False
+enable_Hammer_decode = True
+enable_Hammer_loopback = False
+enable_compare  = False # just a debug that worked for a while, no use for final circuit because it gets wired differenly
+Scrambling_gPEAC_direct = True
+Scrambling_loopback = False
+
 import cocotb
 from cocotb.clock import Clock
 from cocotb.triggers import ClockCycles
 
+# outputs:
+Dout_8     =   1
+QEN        =   2
+CLK_out    =   4
+Zero       =   8
+#inputs:
+DEN        =  16
+Encode     =  32
+Decode     =  64
+Din_8      = 128
+
+async def reset_state(dut):
+  dut.rst_n.value = 0
+  await ClockCycles(dut.clk, 3)
+  dut.rst_n.value = 1
+  await ClockCycles(dut.clk, 3)
+
+async def input_parameter(val, mode, dut):
+  dut.ui_in.value = val & 255
+  # MSB and DEN set
+  dut.uio_in.value = mode | DEN | (Din_8 & (val >> 1))
+  await ClockCycles(dut.clk, 1)
+  dut.ui_in.value = (val >> 9) & 255
+  # clear DEN, set MSB
+  dut.uio_in.value = mode | (Din_8 & (val >> 10))
+  await ClockCycles(dut.clk, 1)
+
+async def output_parameter(dut):
+  timeout = 0
+  while (int(dut.uio_out.value[1])) != 1:
+    timeout = timeout + 1
+    if timeout > 10:
+      print("timeout !")
+      assert false
+      return -1
+    await ClockCycles(dut.clk, 1)
+  #print("waited " + str(timeout))  
+  # LSB first:
+  #print("phase0: " + str(dut.uio_out.value[0]) + " : " + str(dut.uo_out.value))
+  val = int(dut.uo_out.value) + (int(dut.uio_out.value[0]) << 8)
+  #print("uo=" + bin(int(dut.uo_out.value)) + "   uio=" + bin(int(dut.uio_out.value))  + "   QEN=" + str(dut.uio_out.value[1]))
+  await ClockCycles(dut.clk, 1)
+  # print("phase1: " + str(dut.uio_out.value) + " : " + str(dut.uo_out.value))
+  assert dut.uio_out.value[1] == 0
+  #print("uo=" + bin(int(dut.uo_out.value)) + "   uio=" + bin(int(dut.uio_out.value))  + "   QEN=" + str(dut.uio_out.value[1]))
+  return val + (( int(dut.uo_out.value) + (int(dut.uio_out.value[0])<<8)) << 9)
+
+
+# Test vectors for a direct Hammer18 lookup.
+vectors = [
+["111111111111111111", "101011111000110100"],
+["000000000000000000", "000000000000000000"],
+["000000000000000001", "110111011111011001"],
+["000000000000000010", "110111011111011111"],
+["000000000000000100", "110111101111011111"],
+["000000000000001000", "110111101110011111"],
+["000000000000010000", "010111100110110101"],
+["000000000000100000", "110001101011001010"],
+["000000000001000000", "000000011111111000"],
+["000000000010000000", "101110111001000000"],
+["000000000100000000", "111111000010100000"],
+["000000001000000000", "111111011111111001"],
+["000000010000000000", "111110111111111001"],
+["000000100000000000", "111110111111110101"],
+["000001000000000000", "001101111111010101"],
+["000010000000000000", "000111100010110101"],
+["000100000000000000", "110111000000100110"],
+["001000000000000000", "111111000000101100"],
+["010000000000000000", "100001001101110000"],
+["100000000000000000", "000000011111111010"],
+["111111111000000000", "011100010000010101"],
+["000000000111111111", "110111101000100001"],
+["101010101010101010", "100110111110100101"],
+["010101010101010101", "001101000110010001"],
+["110111011111011001", "011011011100100111"],
+["110001101011001010", "000011011010101011"],
+["000000011111111010", "110110111000100111"],
+["011011011100100111", "101100110110001111"],
+["001101111111010101", "111100010010000001"],
+["011011010110110101", "011101101001011100"],
+["100100101001001010", "110110010001101000"],
+["110110010001101000", "101001010101001101"],
+["111100010010000001", "001110001011100000"],
+["110110111000100111", "101000101011111111"],
+["100110111110100101", "101111000010110000"],
+["000011011010101011", "100011000001110011"],
+["101000101011111111", "110110010101011011"],
+["100011000001110011", "101100010000011011"],
+["001110001011100000", "101111110000110100"]]
+
+sequence = [ # for the Hammer18 tests
+ 10,      # index=10  ( out=in because the delay register is cleared)
+ 3147,
+ 228245,
+ 77,
+ 227223,
+ 227216,
+ 86,
+ 96676,
+ 134782,
+ 134777 ]  # index=19
+
+# "Golden" values output by VHDL when encoding 0
+
+# ./gpeac18_scrambler_vectors --vcd=vectors.vcd -gPASSTHROUGH=1 -gvectors=30 |sed 's/.*: //'
+gPEAC_vectors=[
+"011011010101101101", 
+"001010000011100010", 
+"100101011001010000", 
+"101111011100110010", 
+"010101110101000000", 
+"000110010000110001", 
+"011100000101110010", 
+"100010010110100011", 
+"111110011100010101", 
+"100001110001110110", 
+"100001001101001010", 
+"000011111101111111", 
+"100101001011001010", 
+"101001001001001001", 
+"001111010011010001", 
+"111000011100011011", 
+"001000101110101010", 
+"000010001010000100", 
+"001010111000101111", 
+"001101000010110011", 
+"010111111011100010", 
+"100100111110010101", 
+"111100111001110111", 
+"100010110111001010", 
+"100000110000000000", 
+"000100100110001001", 
+"100101010110001010", 
+"101001111100010011", 
+"010000010001011011", 
+"111010001101101111"
+];
+
+Scrambler_vectors=[
+"011011010101101101",
+"011001000100101100",
+"110101101001100100",
+"000011100101110110",
+"111000000111010000",
+"100010111111100110",
+"000011001010111110",
+"100101010100100001",
+"100111001110001111",
+"100110110101010000",
+"001010001111101110",
+"001110101110110001",
+"001001011000111110",
+"000010100001000001",
+"000010000010010101",
+"001010100001011100",
+"001000010110000000",
+"011100001011001110",
+"101011010011111100",
+"111011010101010011",
+"011010110000000100",
+"101111100011111011",
+"110010010100001101",
+"100101000111100001",
+"110110011000011011",
+"000100111001111111",
+"111101100011111111",
+"111100101001000011",
+"110101001010111110",
+"000101101000110010",
+"100011000110110110",
+"010100000010000101",
+"101011001110011111",
+"100111001111110000",
+"111100101010100101",
+"101101101101110001",
+"001110101100111101",
+"011111011011001010",
+"001101110111010111",
+"010001011110100010",
+"011110100110010100",
+"001011001000110001",
+"111110010101000111",
+"001010111011010100",
+"111101101010010111",
+"001110110111010111",
+"001100011011111010",
+"011000011000011001",
+"110111110001111001",
+"100010110000101111"]
+
 
 @cocotb.test()
 async def test_project(dut):
-    dut._log.info("Start")
+  dut._log.info("Start")
 
-    # Set the clock period to 10 us (100 KHz)
-    clock = Clock(dut.clk, 10, unit="us")
-    cocotb.start_soon(clock.start())
+  # Set the clock period to 10 us (100 KHz)
+  clock = Clock(dut.clk, 10, unit="us")
+  cocotb.start_soon(clock.start())
 
-    # Reset
-    dut._log.info("Reset")
-    dut.ena.value = 1
-    dut.ui_in.value = 0
-    dut.uio_in.value = 0
-    dut.rst_n.value = 0
-    await ClockCycles(dut.clk, 10)
-    dut.rst_n.value = 1
+  # Reset
+  dut._log.info("Reset")
+  dut.ena.value = 1
+  dut.ui_in.value = 0
+  dut.uio_in.value = 0
+  
+  # Test bypass mode (mode=0)
+  if enable_bypass == True:
+    await reset_state(dut)  
+    dut._log.info("Starting bypass Mode")
+    for x in vectors:
+      i = int(x[0],2)
+      #print("testing      " + x[0]);
+      await input_parameter(i, 0, dut)  # Encode = Decode = 0 => direct mode
+      o = await output_parameter(dut)
+      #print(" -found " + bin(o + (1 << 20)))
+      assert i == o
+    await ClockCycles(dut.clk, 6)
 
-    dut._log.info("Test project behavior")
+  #  Test Hammer in encode mode (mode=Encode)
+  if enable_Hammer_encode == True:
+    await reset_state(dut)  
+    dut._log.info("Starting Hammer Encode Mode")
+    i = 10
+    for x in sequence:
+      await input_parameter(i, Encode, dut)
+      t = await output_parameter(dut)
+      print(str(i) + " : " + str(t) + "   expected "+ str(x))
+      assert t == x
+      i = i+1
 
-    # Set the input values you want to test
-    dut.ui_in.value = 20
-    dut.uio_in.value = 30
+  # Test Hammer in mode=Decode
+  if enable_Hammer_decode == True:
+    await reset_state(dut)  
+    dut._log.info("Starting Hammer Decode Mode")
+    i = 10
+    for x in sequence:
+      await input_parameter(x, Decode, dut)
+      t = await output_parameter(dut)
+      print(str(i) + " : " + str(t))
+      assert t == i
+      i = i+1
 
-    # Wait for one clock cycle to see the output values
-    await ClockCycles(dut.clk, 1)
+  # Test Hammer in mode=Loopback
+  if enable_Hammer_loopback == True:
+    await reset_state(dut)  
+    dut._log.info("Starting Loopback Mode")
+    for x in sequence:
+      await input_parameter(x, 0, dut)
+      t = await output_parameter(dut)
+      print(str(x) + " -> " + str(t))
+      assert t == x
 
-    # The following assersion is just an example of how to check the output values.
-    # Change it to match the actual expected output of your module:
-    assert dut.uo_out.value == 50
+  # mode=loopback but testing the comparator   /!\  Modulus Comparator is not exposed anymore   /!\
+  if enable_compare == True:
+    await ClockCycles(dut.clk, 6)
+    dut._log.info("Starting Comparator Mode")
+    await reset_state(dut)
+    for i in range(256000, 262144, 2):
+      await input_parameter(i, Decode+Encode, dut)
+      print(str(i) + " : " + str(dut.uio_out.value[3]))
+      # the Zero flag should be 1 when i >= 258144
+      # but there is a delay so it's checked in the VCD
+      # ænyway it's only a temporary test that will not work later
+    await ClockCycles(dut.clk, 6)
 
-    # Keep testing the module by changing the input values, waiting for
-    # one or more clock cycles, and asserting the expected output values.
+
+  ######################################################################
+  # The following code is used to test the whole Hammer+gPEAC circuit
+
+  if Scrambling_gPEAC_direct == True:
+    await reset_state(dut)  
+    dut._log.info("Scrambling Mode")
+    for x in gPEAC_vectors:
+      v = int(x,2)
+      print("expected " + bin(v + (1 << 20)))
+      await input_parameter(0, Encode, dut)  # Encode mode
+      o = await output_parameter(dut)
+      print(" - found " + bin(o + (1 << 20)))
+      #assert v == o
+    await ClockCycles(dut.clk, 6)
+
+  if Scrambling_loopback == True:
+    await reset_state(dut)  
+    dut._log.info("Scrambling/descrambling Mode")
+    for x in range(0, 20):
+      #v = int(x,2)
+      #print("testing " + x[0] + " => " + x[1]);
+      await input_parameter(0, 0, dut)  # loopback mode
+      o = await output_parameter(dut)
+      print("found " + bin(o + (1 << 20)))
+      #assert v == o
+    await ClockCycles(dut.clk, 6)
+
+  
+  await ClockCycles(dut.clk, 6)
+  dut._log.info("Done.")
